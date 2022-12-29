9c42d1871d4265abc15d6ba441089da22ac6a4d0
madda94 <68596431+madda94@users.noreply.github.com>
2022-12-29 16:47:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const art4 = document.getElementById("art-4")
+const art5 = document.getElementById("art-5")
+const art6 = document.getElementById("art-6")
+
+document.getElementById("view-more").addEventListener("click", function() {
+    document.getElementById("art-4").style.display = "block"
+    document.getElementById("art-5").style.display = "block"
+    document.getElementById("art-6").style.display = "block"
+})
+
+
+
+
